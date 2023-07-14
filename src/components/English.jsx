1d6cfc71124a01ecc5921b24faebf8542e7c8e96
ivemobileapp6 <124156205+ivemobileapp6@users.noreply.github.com>
2023-07-14 11:56:02 +0000

    Vocab section is improved with Delete, Login is added with admin code

--- src/components/English.jsx
+++ src/components/English.jsx
@@ -1,8 +1,164 @@
+// import React, { useState, useEffect } from 'react';
+// import { Container, Row, Col, Form, Button, Table, Card } from 'react-bootstrap';
+// import { collection, addDoc, onSnapshot, query, orderBy, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+// import { firestore } from '../Firebase';
+// import BadWords from 'bad-words';
+
+// const filter = new BadWords();
+
+// const EnglishTutor = () => {
+//   const [words, setWords] = useState([]);
+//   const [word, setWord] = useState('');
+//   const [definition, setDefinition] = useState('');
+//   const [currentDefinition, setCurrentDefinition] = useState(null);
+//   const [userAnswer, setUserAnswer] = useState('');
+//   const [feedback, setFeedback] = useState('');
+
+//   const wordsRef = collection(firestore, 'words');
+
+//   useEffect(() => {
+//     const unsubscribe = onSnapshot(query(wordsRef, orderBy('word')), (snapshot) => {
+//       setWords(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+//     });
+
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, []);
+
+//   const addWord = async (e) => {
+//     e.preventDefault();
+
+//     const wordExists = words.some(entry => entry.word.toLowerCase() === word.toLowerCase());
+//     if (wordExists) {
+//       alert("This word already exists.");
+//       return;
+//     }
+
+//     const isBadWord = filter.isProfane(word.toLowerCase());
+//     if (isBadWord) {
+//       alert('This word is not allowed.');
+//       return;
+//     }
+
+//     await addDoc(wordsRef, { word, definition });
+//     setWord('');
+//     setDefinition('');
+//   };
+
+//   const startPractice = () => {
+//     const randomWord = words[Math.floor(Math.random() * words.length)];
+//     setCurrentDefinition(randomWord.definition);
+//   };
+
+//   const checkAnswer = (e) => {
+//     e.preventDefault();
+//     const correctWord = words.find((entry) => entry.definition.toLowerCase() === currentDefinition.toLowerCase());
+//     if (userAnswer.toLowerCase() === correctWord.word.toLowerCase()) {
+//       setFeedback('Correct!');
+//     } else {
+//       setFeedback(`Incorrect. The correct answer is: ${correctWord.word}`);
+//     }
+//   };
+
+
+//   return (
+//     <Container>
+//       <h2>English Tutor</h2>
+//       <Row>
+//         <Col>
+//           <h3>Add Word</h3>
+//           <Form onSubmit={addWord}>
+//             <Form.Group>
+//               <Form.Label>Word</Form.Label>
+//               <Form.Control
+//                 type="text"
+//                 value={word}
+//                 onChange={(e) => setWord(e.target.value)}
+//                 required
+//               />
+//             </Form.Group>
+//             <Form.Group>
+//               <Form.Label>Definition</Form.Label>
+//               <Form.Control
+//                 type="text"
+//                 value={definition}
+//                 onChange={(e) => setDefinition(e.target.value)}
+//                 required
+//               />
+//             </Form.Group>
+//             <Button variant="primary" type="submit">
+//               Add Word
+//             </Button>
+//           </Form>
+//           <h3>Words List</h3>
+//           <Table striped bordered hover size="sm">
+//             <thead>
+//               <tr>
+//                 <th>Word</th>
+//                 <th>Definition</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {words.map((entry, index) => (
+//                 <tr key={index}>
+//                   <td>{entry.word}</td>
+//                   <td>{entry.definition}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </Table>
+//         </Col>
+//         <Col>
+//           <h3>Practice</h3>
+//           <Button onClick={startPractice} disabled={words.length === 0}>
+//             Start Practice
+//           </Button>
+//           {currentDefinition && (
+//             <>
+//               <Card className="mt-3">
+//                 <Card.Body>
+//                   <Card.Title>What is the word with the following definition?</Card.Title>
+//                   <p>{currentDefinition}</p>
+//                   <Form onSubmit={checkAnswer}>
+//                     <Form.Group>
+//                       <Form.Control
+//                         type="text"
+//                         value={userAnswer}
+//                         onChange={(e) => setUserAnswer(e.target.value)}
+//                         required
+//                       />
+//                     </Form.Group>
+//                     <Button variant="primary" type="submit">
+//                       Check Answer
+//                     </Button>
+//                   </Form>
+//                 </Card.Body>
+//               </Card>
+//               {feedback && (
+//                 <div className="mt-3">
+//                   <h4>Feedback</h4>
+//                   <p>{feedback}</p>
+//                 </div>
+//               )}
+//             </>
+//           )}
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default EnglishTutor;
+
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button, Table, Card } from 'react-bootstrap';
-import { collection, addDoc, onSnapshot, query, orderBy, doc, deleteDoc, updateDoc } from 'firebase/firestore';
-import { firestore } from '../Firebase';
+import { collection, addDoc, onSnapshot, query, orderBy, doc, deleteDoc, updateDoc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { firestore, auth } from '../Firebase';
 import BadWords from 'bad-words';
+
+const filter = new BadWords();
 
 const EnglishTutor = () => {
   const [words, setWords] = useState([]);
@@ -11,6 +167,7 @@ const EnglishTutor = () => {
   const [currentDefinition, setCurrentDefinition] = useState(null);
   const [userAnswer, setUserAnswer] = useState('');
   const [feedback, setFeedback] = useState('');
+  const [userRole, setUserRole] = useState(null);
 
   const wordsRef = collection(firestore, 'words');
 
@@ -24,19 +181,51 @@ const EnglishTutor = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userRef = doc(collection(firestore, 'users'), user.uid);
+        const userDocSnap = await getDoc(userRef);
+        const userData = userDocSnap.data();
+        setUserRole(userData?.role);
+      } else {
+        setUserRole(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const addWord = async (e) => {
     e.preventDefault();
 
     const wordExists = words.some(entry => entry.word.toLowerCase() === word.toLowerCase());
     if (wordExists) {
-       alert("This word already exists.");
-       return;
-  }
+      alert("This word already exists.");
+      return;
+    }
 
-  await addDoc(wordsRef, { word, definition });
-  setWord('');
-  setDefinition('');
-};
+    const isBadWord = filter.isProfane(word.toLowerCase());
+    if (isBadWord) {
+      alert('This word is not allowed.');
+      return;
+    }
+
+    await addDoc(wordsRef, { word, definition });
+    setWord('');
+    setDefinition('');
+  };
+
+  const deleteWord = async (wordId) => {
+    try {
+      await deleteDoc(doc(wordsRef, wordId));
+      setWords(words.filter((word) => word.id !== wordId));
+    } catch (error) {
+      console.error('Error deleting word:', error);
+    }
+  };
 
   const startPractice = () => {
     const randomWord = words[Math.floor(Math.random() * words.length)];
@@ -89,6 +278,7 @@ const EnglishTutor = () => {
               <tr>
                 <th>Word</th>
                 <th>Definition</th>
+                {userRole === 'admin' && <th>Delete</th>}
               </tr>
             </thead>
             <tbody>
@@ -96,6 +286,13 @@ const EnglishTutor = () => {
                 <tr key={index}>
                   <td>{entry.word}</td>
                   <td>{entry.definition}</td>
+                  {userRole === 'admin' && (
+                    <td>
+                      <Button variant="danger" onClick={() => deleteWord(entry.id)}>
+                        Delete
+                      </Button>
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,146 @@
+// import React, { useEffect, useState } from 'react';
+// import { auth, googleProvider, firestore } from '../Firebase';
+// import { signInWithPopup, signOut } from 'firebase/auth';
+// import { collection, doc, setDoc, getDoc } from 'firebase/firestore';
+// import { onAuthStateChanged } from 'firebase/auth';
+
+// const saveUserDetails = async (user, setIsNewUser) => {
+//   const userRef = doc(collection(firestore, 'users'), user.uid);
+//   const userDocSnap = await getDoc(userRef);
+
+//   if (!userDocSnap.exists()) {
+//     setIsNewUser(true);
+
+//     // Set the role to "student" for new users only
+//     await setDoc(userRef, {
+//       ...userDocSnap.data(),
+//       role: 'student',
+//     });
+//   } else {
+//     setIsNewUser(false);
+//   }
+
+//   return userRef;
+// };
+
+// const Login = (props) => {
+//   const [user, setUser] = useState(null);
+//   const [isNewUser, setIsNewUser] = useState(false);
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [age, setAge] = useState('');
+//   const [gender, setGender] = useState('');
+//   const [address, setAddress] = useState('');
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       setUser(user);
+//     });
+
+//     return () => {
+//       unsubscribe();
+//     };
+//   }, []);
+
+//   const signInWithGoogle = async () => {
+//     try {
+//       const result = await signInWithPopup(auth, googleProvider);
+//       const user = result.user;
+//       await saveUserDetails(user, setIsNewUser);
+//       props.onLoginSuccess(); 
+//     } catch (error) {
+//       console.error('Error signing in with Google:', error);
+//     }
+//   };
+
+//   const logout = async () => {
+//     try {
+//       await signOut(auth);
+//       setUser(null);
+//     } catch (error) {
+//       console.error('Error signing out:', error);
+//     }
+//   };
+
+//   const submitAdditionalInfo = async (e) => {
+//     e.preventDefault();
+
+//     if (user) {
+//       const userRef = doc(collection(firestore, 'users'), user.uid);
+//       const createdAt = new Date();
+//       const { displayName, email } = user;
+
+//       await setDoc(userRef, {
+//         displayName,
+//         email,
+//         createdAt,
+//         phoneNumber,
+//         age,
+//         gender,
+//         address,
+//         role: 'student', // Include the role when saving the additional information
+//       });
+//     }
+
+//     setIsNewUser(false);
+//   };
+
+//   return (
+//     <div>
+//       {user ? (
+//         isNewUser ? (
+//           <form onSubmit={submitAdditionalInfo}>
+//             <label>
+//               Phone Number:
+//               <input
+//                 type="text"
+//                 value={phoneNumber}
+//                 onChange={(e) => setPhoneNumber(e.target.value)}
+//               />
+//             </label>
+//             <label>
+//               Age:
+//               <input
+//                 type="number"
+//                 value={age}
+//                 onChange={(e) => setAge(e.target.value)}
+//               />
+//             </label>
+//             <label>
+//               Gender:
+//               <select value={gender} onChange={(e) => setGender(e.target.value)}>
+//                 <option value="">Select</option>
+//                 <option value="male">Male</option>
+//                 <option value="female">Female</option>
+//                 <option value="other">Other</option>
+//               </select>
+//             </label>
+//             <label>
+//               Address:
+//               <input
+//                 type="text"
+//                 value={address}
+//                 onChange={(e) => setAddress(e.target.value)}
+//               />
+//             </label>
+//             <button type="submit">Submit</button>
+//           </form>
+//         ) : (
+//           <>
+//             <p>
+//               Welcome, {user.displayName} ({user.email})
+//             </p>
+//             <button onClick={logout}>Sign Out</button>
+//           </>
+//         )
+//       ) : (
+//         <button onClick={signInWithGoogle}>Sign in with Google</button>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Login;
+
 import React, { useEffect, useState } from 'react';
 import { auth, googleProvider, firestore } from '../Firebase';
 import { signInWithPopup, signOut } from 'firebase/auth';
@@ -10,12 +153,6 @@ const saveUserDetails = async (user, setIsNewUser) => {
 
   if (!userDocSnap.exists()) {
     setIsNewUser(true);
-
-    // Set the role to "student" for new users only
-    await setDoc(userRef, {
-      ...userDocSnap.data(),
-      role: 'student',
-    });
   } else {
     setIsNewUser(false);
   }
@@ -30,6 +167,8 @@ const Login = (props) => {
   const [age, setAge] = useState('');
   const [gender, setGender] = useState('');
   const [address, setAddress] = useState('');
+  const [role, setRole] = useState('student');
+  const [adminCode, setAdminCode] = useState('');
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -46,7 +185,7 @@ const Login = (props) => {
       const result = await signInWithPopup(auth, googleProvider);
       const user = result.user;
       await saveUserDetails(user, setIsNewUser);
-      props.onLoginSuccess(); 
+      props.onLoginSuccess();
     } catch (error) {
       console.error('Error signing in with Google:', error);
     }
@@ -64,6 +203,11 @@ const Login = (props) => {
   const submitAdditionalInfo = async (e) => {
     e.preventDefault();
 
+    if (role === 'admin' && adminCode !== '123456') {
+      alert('Incorrect admin code.');
+      return;
+    }
+
     if (user) {
       const userRef = doc(collection(firestore, 'users'), user.uid);
       const createdAt = new Date();
@@ -77,7 +221,7 @@ const Login = (props) => {
         age,
         gender,
         address,
-        role: 'student', // Include the role when saving the additional information
+        role,
       });
     }
 
@@ -122,6 +266,23 @@ const Login = (props) => {
                 onChange={(e) => setAddress(e.target.value)}
               />
             </label>
+            <label>
+              Role:
+              <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value="student">Student</option>
+                <option value="admin">Admin</option>
+              </select>
+            </label>
+            {role === 'admin' && (
+              <label>
+                Admin Code:
+                <input
+                  type="password"
+                  value={adminCode}
+                  onChange={(e) => setAdminCode(e.target.value)}
+                />
+              </label>
+            )}
             <button type="submit">Submit</button>
           </form>
         ) : (
